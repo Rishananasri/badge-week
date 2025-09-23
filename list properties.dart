ce7af7862898febfe2d075a5c771add5f930a2a3
Rishananasri <rishananasri203@gmail.com>
2025-09-23 09:41:07 +0530

    Create list properties.dart

---
+++ list properties.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<int> a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print(a.length);
+  print(a.isEmpty);
+  print(a.isNotEmpty);
+  print(a.first);
+  print(a.last);
+  print(a.reversed);
+}
