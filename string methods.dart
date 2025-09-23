cf7d449bf0f7dfea5ac77470b991671243d254ac
Rishananasri <rishananasri203@gmail.com>
2025-09-23 09:39:52 +0530

    Create string methods.dart

---
+++ string methods.dart
@@ -0,0 +1,8 @@
+void main() {
+  String a = "Rishuu";
+  print(a.toUpperCase());
+  print(a.toLowerCase());
+  print(a.contains("sh"));
+  print(a.substring(0, 4));
+  print(a.replaceAll("uu", ".."));
+}
