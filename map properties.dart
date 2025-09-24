664495a0246f05f7153b4298a700272da67ae7a3
Rishananasri <rishananasri203@gmail.com>
2025-09-24 17:20:05 +0530

    Create map properties.dart

---
+++ map properties.dart
@@ -0,0 +1,8 @@
+void main() {
+  Map<int, String> a = {1: "apple", 2: "grapes", 3: "orange", 4: "kiwi"};
+  print(a.isEmpty);
+  print(a.isNotEmpty);
+  print(a.length);
+  print(a.keys);
+  print(a.values);
+}
